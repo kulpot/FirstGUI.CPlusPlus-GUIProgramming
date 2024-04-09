4263f2aaa208ebc32a6dec59dc21aaee64a08bb8
Kulpot <kulpotwahu@gmail.com>
2024-04-09 19:36:34 +0800

    App.cpp-> bool App::OnInit() { ->  mainFrame->Center();

--- FirstGUI/App.cpp
+++ FirstGUI/App.cpp
@@ -7,6 +7,7 @@ wxIMPLEMENT_APP(App);
 bool App::OnInit() {
 	MainFrame* mainFrame = new MainFrame("C++ GUI");
 	mainFrame->SetClientSize(800, 600);
+	mainFrame->Center();
 	mainFrame->Show();
 	return true;
 }
