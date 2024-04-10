e8abc1d0c53929638c432ce15e07a7329b54f2a5
Kulpot <kulpotwahu@gmail.com>
2024-04-10 18:04:57 +0800

    START: Ep3 Adding Controls ref link:https://www.youtube.com/watch?v=_2FMt_0wVMM&list=PLFk1_lkqT8MbVOcwEppCPfjGOGhLvcf9G&index=4 MainFrame.cpp->  wxButton* button = new wxButton(this, wxID_ANY, "Button", wxPoint(150, 50), wxSize(100, 35));

--- FirstGUI/MainFrame.cpp
+++ FirstGUI/MainFrame.cpp
@@ -2,5 +2,5 @@
 #include <wx/wx.h>
 
 MainFrame::MainFrame(const wxString& title): wxFrame(nullptr, wxID_ANY, title) {
-
+	wxButton* button = new wxButton(this, wxID_ANY, "Button", wxPoint(150, 50), wxSize(100, 35));
 }
