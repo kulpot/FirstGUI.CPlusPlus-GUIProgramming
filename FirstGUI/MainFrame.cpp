3e8f93e5d05254b96840da8e2472ee5c7d35be20
Kulpot <kulpotwahu@gmail.com>
2024-04-12 19:40:07 +0800

    MainFrame.cpp-> styles-> wxBU_LEFT->  wxButton* button = new wxButton(panel, wxID_ANY, "Button", wxPoint(150, 50), wxSize(100, 35), wxBU_LEFT);

--- FirstGUI/MainFrame.cpp
+++ FirstGUI/MainFrame.cpp
@@ -5,7 +5,7 @@
 MainFrame::MainFrame(const wxString& title): wxFrame(nullptr, wxID_ANY, title) {
 	wxPanel* panel = new wxPanel(this);
 
-	wxButton* button = new wxButton(panel, wxID_ANY, "Button", wxPoint(150, 50), wxSize(100, 35));
+	wxButton* button = new wxButton(panel, wxID_ANY, "Button", wxPoint(150, 50), wxSize(100, 35), wxBU_LEFT);
 	
 	wxCheckBox* checkBox = new wxCheckBox(panel, wxID_ANY, "CheckBox", wxPoint(550, 55));
 
