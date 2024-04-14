8acd3bb1bf26ff6c40a314f8dfd62469dc62288b
Kulpot <kulpotwahu@gmail.com>
2024-04-14 17:31:51 +0800

    MainFrame.cpp->  wxButton* button = new wxButton(panel, BUTTON_ID, "Button", wxPoint(300, 275), wxSize(200, 50));

--- FirstGUI/MainFrame.cpp
+++ FirstGUI/MainFrame.cpp
@@ -16,6 +16,8 @@ enum IDs {
 MainFrame::MainFrame(const wxString& title): wxFrame(nullptr, wxID_ANY, title) {
 	wxPanel* panel = new wxPanel(this);
 
+	wxButton* button = new wxButton(panel, BUTTON_ID, "Button", wxPoint(300, 275), wxSize(200, 50));
+
 	//wxButton* button = new wxButton(panel, wxID_ANY, "Button", wxPoint(150, 50), wxSize(100, 35), wxBU_LEFT);
 	//
 	//wxCheckBox* checkBox = new wxCheckBox(panel, wxID_ANY, "CheckBox",
