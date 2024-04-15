df07f2c0a12cee352c830eb0d7b573369b08b5e6
Kulpot <kulpotwahu@gmail.com>
2024-04-16 05:34:12 +0800

    START:Ep 7 Event Propagation ref link:https://www.youtube.com/watch?v=55swdnpaSRs&list=PLFk1_lkqT8MbVOcwEppCPfjGOGhLvcf9G&index=8

--- FirstGUI/MainFrame.cpp
+++ FirstGUI/MainFrame.cpp
@@ -28,14 +28,14 @@ MainFrame::MainFrame(const wxString& title): wxFrame(nullptr, wxID_ANY, title) {
 	//wxSlider* slider = new wxSlider(panel, SLIDER_ID, 0, 0, 100, wxPoint(300, 200), wxSize(200, -1));
 	//wxTextCtrl* text = new wxTextCtrl(panel, TEXT_ID, "", wxPoint(300, 375), wxSize(200, -1));
 	wxButton* button = new wxButton(panel, wxID_ANY, "Button", wxPoint(300, 275), wxSize(200, 50));
-	wxSlider* slider = new wxSlider(panel, wxID_ANY, 0, 0, 100, wxPoint(300, 200), wxSize(200, -1));
-	wxTextCtrl* text = new wxTextCtrl(panel, wxID_ANY, "", wxPoint(300, 375), wxSize(200, -1));
-
+	//wxSlider* slider = new wxSlider(panel, wxID_ANY, 0, 0, 100, wxPoint(300, 200), wxSize(200, -1));
+	//wxTextCtrl* text = new wxTextCtrl(panel, wxID_ANY, "", wxPoint(300, 375), wxSize(200, -1));
+	//
 	button->Bind(wxEVT_BUTTON, &MainFrame::OnButtonClicked, this);
-	slider->Bind(wxEVT_SLIDER, &MainFrame::OnSliderChanged, this);
-	text->Bind(wxEVT_TEXT, &MainFrame::OnTextChanged, this);
-
-	button->Unbind(wxEVT_BUTTON, &MainFrame::OnButtonClicked, this);
+	//slider->Bind(wxEVT_SLIDER, &MainFrame::OnSliderChanged, this);
+	//text->Bind(wxEVT_TEXT, &MainFrame::OnTextChanged, this);
+	//
+	//button->Unbind(wxEVT_BUTTON, &MainFrame::OnButtonClicked, this);
 
 	CreateStatusBar();
 
@@ -85,12 +85,12 @@ void MainFrame::OnButtonClicked(wxCommandEvent& evt) {
 	wxLogStatus("Button Clicked");
 }
 
-void MainFrame::OnSliderChanged(wxCommandEvent& evt) {
-	wxString str = wxString::Format("Slider Value: %d", evt.GetInt());
-	wxLogStatus(str);
-}
-
-void MainFrame::OnTextChanged(wxCommandEvent& evt) {
-	wxString str = wxString::Format("Text: %s", evt.GetString());
-	wxLogStatus(str);
-}
+//void MainFrame::OnSliderChanged(wxCommandEvent& evt) {
+//	wxString str = wxString::Format("Slider Value: %d", evt.GetInt());
+//	wxLogStatus(str);
+//}
+//
+//void MainFrame::OnTextChanged(wxCommandEvent& evt) {
+//	wxString str = wxString::Format("Text: %s", evt.GetString());
+//	wxLogStatus(str);
+//}
