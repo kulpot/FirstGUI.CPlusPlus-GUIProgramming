130c82f002efbd6331d74b362563f64046a01914
Kulpot <kulpotwahu@gmail.com>
2024-04-17 20:11:45 +0800

    MainFrame.h->  void OnAnyButtonClicked(wxCommandEvent& evt);

--- FirstGUI/MainFrame.h
+++ FirstGUI/MainFrame.h
@@ -7,7 +7,8 @@ class MainFrame	: public wxFrame
 public:
 	MainFrame(const wxString& title);
 private:
-	void OnButtonClicked(wxCommandEvent& evt);
+	//void OnButtonClicked(wxCommandEvent& evt);
+	void OnAnyButtonClicked(wxCommandEvent& evt);
 	void OnButton1Clicked(wxCommandEvent& evt);
 	void OnButton2Clicked(wxCommandEvent& evt);
 	//void OnSliderChanged(wxCommandEvent& evt);
