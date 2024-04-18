6000ededb8c1c1a95a82571a53fbde28a07f6732
Kulpot <kulpotwahu@gmail.com>
2024-04-18 20:55:06 +0800

    MainFrame.h-> void OnMouseEvent(wxMouseEvent& evt);

--- FirstGUI/MainFrame.h
+++ FirstGUI/MainFrame.h
@@ -7,13 +7,17 @@ class MainFrame	: public wxFrame
 public:
 	MainFrame(const wxString& title);
 private:
-	//void OnButtonClicked(wxCommandEvent& evt);
-	void OnAnyButtonClicked(wxCommandEvent& evt);
-	void OnButton1Clicked(wxCommandEvent& evt);
-	void OnButton2Clicked(wxCommandEvent& evt);
-	//void OnSliderChanged(wxCommandEvent& evt);
-	//void OnTextChanged(wxCommandEvent& evt);
-	//wxDECLARE_EVENT_TABLE(); //static event handling
-	void OnClose(wxCloseEvent& evt);
+	void OnMouseEvent(wxMouseEvent& evt);
+
+
+
+//	//void OnButtonClicked(wxCommandEvent& evt);
+//	void OnAnyButtonClicked(wxCommandEvent& evt);
+//	void OnButton1Clicked(wxCommandEvent& evt);
+//	void OnButton2Clicked(wxCommandEvent& evt);
+//	//void OnSliderChanged(wxCommandEvent& evt);
+//	//void OnTextChanged(wxCommandEvent& evt);
+//	//wxDECLARE_EVENT_TABLE(); //static event handling
+//	void OnClose(wxCloseEvent& evt);
 };
 
