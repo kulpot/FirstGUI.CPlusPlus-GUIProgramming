18fda3faaa251d112855c0116c4fb682150c5df9
Kulpot <kulpotwahu@gmail.com>
2024-04-19 20:01:40 +0800

    MainFrame.cpp->
     //----------------REF LINK(AllMouseEvents):https://docs.wxwidgets.org/3.0/classwx_mouse_event.html-----------------------
     //panel->Bind(wxEVT_LEFT_DOWN, &MainFrame::OnMouseEvent, this);
     //panel->Bind(wxEVT_RIGHT_DOWN, &MainFrame::OnMouseEvent, this);
     //panel->Bind(wxEVT_MIDDLE_DOWN, &MainFrame::OnMouseEvent, this);
     //panel->Bind(wxEVT_LEFT_DCLICK, &MainFrame::OnMouseEvent, this);
     panel->Bind(wxEVT_MOTION, &MainFrame::OnMouseEvent, this);

--- FirstGUI/MainFrame.cpp
+++ FirstGUI/MainFrame.cpp
@@ -34,7 +34,8 @@ MainFrame::MainFrame(const wxString& title): wxFrame(nullptr, wxID_ANY, title) {
 	//panel->Bind(wxEVT_LEFT_DOWN, &MainFrame::OnMouseEvent, this);
 	//panel->Bind(wxEVT_RIGHT_DOWN, &MainFrame::OnMouseEvent, this);
 	//panel->Bind(wxEVT_MIDDLE_DOWN, &MainFrame::OnMouseEvent, this);
-	panel->Bind(wxEVT_LEFT_DCLICK, &MainFrame::OnMouseEvent, this);
+	//panel->Bind(wxEVT_LEFT_DCLICK, &MainFrame::OnMouseEvent, this);
+	panel->Bind(wxEVT_MOTION, &MainFrame::OnMouseEvent, this);
 
 
 	//wxButton* button = new wxButton(panel, BUTTON_ID, "Button", wxPoint(300, 275), wxSize(200, 50));
