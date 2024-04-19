39bd7f5b18720530920b5ca4703987a71b948775
Kulpot <kulpotwahu@gmail.com>
2024-04-20 02:04:42 +0800

    MainFrame.cpp->
     wxButton* button = new wxButton(panel, wxID_ANY, "Button", wxPoint(300, 250), wxSize(200, 100));

--- FirstGUI/MainFrame.cpp
+++ FirstGUI/MainFrame.cpp
@@ -25,6 +25,7 @@
 
 MainFrame::MainFrame(const wxString& title): wxFrame(nullptr, wxID_ANY, title) {
 	wxPanel* panel = new wxPanel(this);
+	wxButton* button = new wxButton(panel, wxID_ANY, "Button", wxPoint(300, 250), wxSize(200, 100));
 
 	//------------------CONSTRUCTOR----------------------------------
 
