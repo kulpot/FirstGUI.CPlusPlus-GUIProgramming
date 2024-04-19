7968818217f2bd93adc7ef0b2a9740e75712ae62
Kulpot <kulpotwahu@gmail.com>
2024-04-20 02:12:08 +0800

    MainFrame.cpp-> mousePos = this->ScreenToClient(mousePos);

--- FirstGUI/MainFrame.cpp
+++ FirstGUI/MainFrame.cpp
@@ -113,6 +113,7 @@ MainFrame::MainFrame(const wxString& title): wxFrame(nullptr, wxID_ANY, title) {
 void MainFrame::OnMouseEvent(wxMouseEvent& evt) {
 	//wxPoint mousePos = evt.GetPosition();
 	wxPoint mousePos = wxGetMousePosition();	// Screen coordinate mouse event motion
+	mousePos = this->ScreenToClient(mousePos);
 	wxString message = wxString::Format("Mouse Event Detected! (x=%d y=%d)", mousePos.x, mousePos.y);
 	//wxLogStatus("Mouse Event Detected!");
 	wxLogStatus(message);
