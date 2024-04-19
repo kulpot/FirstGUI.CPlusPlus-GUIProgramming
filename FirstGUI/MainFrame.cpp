5fb87a082154659cdb7f1928e6e1ddc7b16dfa00
Kulpot <kulpotwahu@gmail.com>
2024-04-19 19:48:19 +0800

    MainFrame.cpp->
    void MainFrame::OnMouseEvent(wxMouseEvent& evt) {
     wxPoint mousePos = evt.GetPosition();
     wxString message = wxString::Format("Mouse Event Detected! (x=%d y=%d)", mousePos.x, mousePos.y);

--- FirstGUI/MainFrame.cpp
+++ FirstGUI/MainFrame.cpp
@@ -103,6 +103,7 @@ MainFrame::MainFrame(const wxString& title): wxFrame(nullptr, wxID_ANY, title) {
 
 void MainFrame::OnMouseEvent(wxMouseEvent& evt) {
 	wxPoint mousePos = evt.GetPosition();
+	wxString message = wxString::Format("Mouse Event Detected! (x=%d y=%d)", mousePos.x, mousePos.y);
 	wxLogStatus("Mouse Event Detected!");
 }
 
