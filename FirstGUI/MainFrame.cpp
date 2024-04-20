586241a60368ff81ef5acb9b9384d2164bea855f
Kulpot <kulpotwahu@gmail.com>
2024-04-21 03:30:55 +0800

    MainFrame.cpp->
    void MainFrame::OnKeyEvent(wxKeyEvent& evt) {
     wxChar keyChar = evt.GetUnicodeKey();

--- FirstGUI/MainFrame.cpp
+++ FirstGUI/MainFrame.cpp
@@ -114,6 +114,7 @@ MainFrame::MainFrame(const wxString& title): wxFrame(nullptr, wxID_ANY, title) {
 }
 
 void MainFrame::OnKeyEvent(wxKeyEvent& evt) {
+	wxChar keyChar = evt.GetUnicodeKey();
 	wxLogStatus("Key Event");
 }
 
